a674e72ad1ea162f482951293f6e1802c91bb240
Iipin <52832022+Iipin@users.noreply.github.com>
2023-03-11 14:36:16 +0100

    invoke swiftformat directly with pre-commit

--- scripts/swiftformat-wrapper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if which swiftformat >/dev/null; then
-  exec scripts/git-format-staged.py --formatter "swiftformat --exclude Package.swift,**/Generated/* stdin --stdinpath '{}'" '*.swift'
-else
-  echo 'warning: swiftformat not installed, run `bundle install`.'
-fi
