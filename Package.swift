7e6e78ccddcd94c0a8cb192d8a7fce3d498d69a6
Iipin <52832022+Iipin@users.noreply.github.com>
2024-01-06 14:20:02 +0100

    Use Airbnb swiftformat tool again

--- Package.swift
+++ Package.swift
@@ -2,54 +2,54 @@
 import PackageDescription
 
 let package = Package(
-    name: "AirbnbSwift",
-    platforms: [.macOS(.v10_13)],
-    products: [
-        .plugin(name: "FormatSwift", targets: ["FormatSwift"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.3"),
-    ],
-    targets: [
-        .plugin(
-            name: "FormatSwift",
-            capability: .command(
-                intent: .custom(
-                    verb: "format",
-                    description: "Formats Swift source files according to the Airbnb Swift Style Guide"
-                ),
-                permissions: [
-                    .writeToPackageDirectory(reason: "Format Swift source files"),
-                ]
-            ),
-            dependencies: [
-                "AirbnbSwiftFormatTool",
-                "SwiftFormat",
-                "SwiftLintBinary",
-            ]
-        ),
-        .executableTarget(
-            name: "AirbnbSwiftFormatTool",
-            dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
-            ],
-            resources: [
-                .process("airbnb.swiftformat"),
-                .process("swiftlint.yml"),
-            ]
-        ),
-        .binaryTarget(
-            name: "SwiftFormat",
-            url: "https://github.com/BugFactoryInc/SwiftFormat/releases/download/0.51.2/SwiftFormat.artifactbundle.zip",
-            checksum: "806b333742a071d8f8478f3ec66e59655c84693bc2154967825448c137dfd096"
-        ),
-        .binaryTarget(
-            name: "SwiftLintBinary",
-            url: "https://github.com/realm/SwiftLint/releases/download/0.48.0/SwiftLintBinary-macos.artifactbundle.zip",
-            checksum: "9c255e797260054296f9e4e4cd7e1339a15093d75f7c4227b9568d63edddba50"
-        ),
-    ]
-)
+  name: "AirbnbSwift",
+  platforms: [.macOS(.v10_13)],
+  products: [
+    .plugin(name: "FormatSwift", targets: ["FormatSwift"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.3"),
+  ],
+  targets: [
+    .plugin(
+      name: "FormatSwift",
+      capability: .command(
+        intent: .custom(
+          verb: "format",
+          description: "Formats Swift source files according to the Airbnb Swift Style Guide"),
+        permissions: [
+          .writeToPackageDirectory(reason: "Format Swift source files"),
+        ]),
+      dependencies: [
+        "AirbnbSwiftFormatTool",
+        "SwiftFormat",
+        "SwiftLintBinary",
+      ]),
+
+    .executableTarget(
+      name: "AirbnbSwiftFormatTool",
+      dependencies: [
+        .product(name: "ArgumentParser", package: "swift-argument-parser"),
+      ],
+      resources: [
+        .process("airbnb.swiftformat"),
+        .process("swiftlint.yml"),
+      ]),
+
+    .testTarget(
+      name: "AirbnbSwiftFormatToolTests",
+      dependencies: ["AirbnbSwiftFormatTool"]),
+
+    .binaryTarget(
+      name: "SwiftFormat",
+      url: "https://github.com/calda/SwiftFormat/releases/download/0.53-beta-8/SwiftFormat.artifactbundle.zip",
+      checksum: "42b2612305c5bffe57102d6c1a2e137309b5cf246638ff9ff75e3260a6795a19"),
+
+    .binaryTarget(
+      name: "SwiftLintBinary",
+      url: "https://github.com/realm/SwiftLint/releases/download/0.53.0/SwiftLintBinary-macos.artifactbundle.zip",
+      checksum: "03416a4f75f023e10f9a76945806ddfe70ca06129b895455cc773c5c7d86b73e"),
+  ])
 
 // Emit an error on Linux, so Swift Package Manager's platform support detection doesn't say this package supports Linux
 // https://github.com/airbnb/swift/discussions/197#discussioncomment-4055303
